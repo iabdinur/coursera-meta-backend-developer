e6405563ac7bd2412da22e8055762372c425cfdd
iabdinur <iabdinur1@gmail.com>
2023-08-01 22:50:36 +0100

    course 6 APIs wk3 project update

---
+++ course6-APIs/wk3/LittleLemon/LittleLemonDRF/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('category', views.CategoriesView.as_view()),
+    path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+]
