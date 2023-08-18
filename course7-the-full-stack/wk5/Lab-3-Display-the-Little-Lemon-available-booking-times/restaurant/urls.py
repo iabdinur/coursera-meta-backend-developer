4cbed89a3aa5bf75f7c7b3a52c34eae79c005a6c
iabdinur <iabdinur1@gmail.com>
2023-08-18 13:14:27 +0100

    course 7 certification

--- course7-the-full-stack/wk5/Lab-3-Display-the-Little-Lemon-available-booking-times/restaurant/urls.py
+++ course7-the-full-stack/wk5/Lab-3-Display-the-Little-Lemon-available-booking-times/restaurant/urls.py
@@ -3,11 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.index, name='home'),
     path('about/', views.about, name="about"),
-    path('book/', views.book, name="book"),
-    path('reservations/', views.reservations, name="reservations"),
-    path('menu/', views.menu, name="menu"),
-    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
-    path('bookings', views.bookings, name='bookings'), 
+    path('menu/', views.menu, name='menu'),
+    path('menu-item/<int:pk>/', views.menu_item, name='menu-detail'),
+    path('book/', views.Book.as_view(), name='book'),
+    path('bookings/', views.bookings, name='bookings'),
 ]
